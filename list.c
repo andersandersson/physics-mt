57e44c7c80fa104536eaae84fb7491e02806c1fb
Anders Andersson <finrist@gmail.com>
2009-05-10 21:50:40 +0200

    Added a linked list struct with helper functions

---
+++ list.c
@@ -0,0 +1,75 @@
+#include "list.h"
+
+#include <stdlib.h>
+
+Node* node_create() {
+  Node* node = malloc(sizeof(Node));
+
+  node->item = NULL;
+  node->next = NULL;
+
+  return node;
+}
+
+void node_free(Node* node) {
+  free(node);
+}
+
+List* list_create() {
+  List* new_list;
+
+  new_list = malloc(sizeof(List));
+
+  new_list->first_node = NULL;
+  new_list->last_node = NULL;
+
+  return new_list;
+}
+
+void list_free(List* list) {
+  Node* node = list->first_node;
+  Node* next_node;
+
+  while(NULL != node) {
+    next_node = node->next;
+
+    free(node);
+
+    node = next_node;
+  }
+
+  free(list);
+}
+
+void list_push(List* list, void* item) {
+  Node* node = node_create();
+  
+  node->next = list->first_node;
+  node->item = item;
+
+  if(NULL == list->first_node) {
+    list->last_node = node;
+  }
+
+  list->first_node = node;
+}
+
+void* list_pop(List* list) {
+  Node* node = list->first_node;
+  void* item = NULL;
+
+  if(NULL != node) {
+    list->first_node = node->next;
+
+    item = node->item;
+
+    node_free(node);
+  }
+
+  if(NULL == list->first_node) {
+    list->last_node = NULL;
+  }
+
+  return item;
+}
+
---
+++ list.h
@@ -0,0 +1,22 @@
+#ifndef LIST_H
+#define LIST_H
+
+typedef struct Node {
+  void* item;
+  struct Node* next;
+} Node;
+
+typedef struct List {
+  Node* first_node;
+  Node* last_node;
+} List;
+
+Node* node_create();
+void  node_free(Node* node);
+
+List* list_create();
+void  list_free(List* list);
+void  list_push(List* list, void* item);
+void* list_pop(List* list);
+
+#endif
